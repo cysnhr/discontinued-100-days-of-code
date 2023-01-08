b12792414057f5e046f168948a467e121535cd35
Giselle <113964879+cysnhr@users.noreply.github.com>
2023-01-08 20:59:39 +0800

    Update for_loop.py

--- codingpractices/for_loop.py
+++ codingpractices/for_loop.py
@@ -10,3 +10,11 @@ for m in range(0,q):
     tmp[new-1] = s[j]
     if j == k-1:
       s = tmp
+
+"""
+question:
+string = apcs
+input 3142
+the first letter in the string goes to the third place in a new string
+I can't remember all of it so I'll have to wait until someone posts it online
+"""
