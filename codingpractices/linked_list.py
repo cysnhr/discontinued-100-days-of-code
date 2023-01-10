952be004a990b526f583eaff443684ac6f9d456b
Giselle <113964879+cysnhr@users.noreply.github.com>
2023-01-10 22:10:36 +0800

    Update linked_list.py

--- codingpractices/linked_list.py
+++ codingpractices/linked_list.py
@@ -1,59 +1,44 @@
-# 20230105
-
-"""
-features of linked list:
-
-- append stuff to the end
-- insert stuff
-- deletion
-- search for
-
-"""
+# 20230110
 
 class Node():
-  def __init__(self, val, next=None):
-    self.val = val
-    self.next = next
+    def __init__(self, val, next=None):
+      self.val = val
+      self.next = next
 
+class ListNode(object):
+    def __init__(self, head=None):
+      self.head = None
 
-class LinkedList():
-  # note that everything should be in the form of nodes before passing into this function
-  def __init__(self, head=None):
-    self.head = None # initialization
+    def __repr__(self):
+      ls = []
+      node = self.head
+      if node is None:
+        return "No nodes present."
+      while node is not None:
+        ls.append(str(node.val))
+        print(f"{node} is appended. Pending: {node.next}")
+        node = node.next
+        continue
+        print(ls)
+      return "->".join(ls)
 
+    def add(self, val):
+      new_node = Node(val)
+      print(f"This is the node you entered: {new_node}: {new_node.val}")
+      node = self.head
+      print(f"This is self.head: {self.head}, and the node appointed to it: {node}")
+      if node is None: # if self.head is None
+        self.head = new_node
+        new_node.next = None
+        print(f"The new self.head is {self.head}!")
+        print(f"New node {new_node} added.")
+      else:
+        while node.next: # while it's not end
+          node = node.next
+        node.next = new_node
+        print(f"Without None self.head, new node {node.next} added, {node.next.val}.")
 
-  def __repr__(self):
-    nodes = []
-    # will be in string form
-    node = self.head
-    print(node.val)
-    # the first value in the first node is head
-    while node: # if the node isn't none
-      nodes.append(node)
-      print(node)
-      node = node.next
-
-    if nodes == []:
-      print("No nodes present.")
-    else:
-      print(nodes)
-      return " -> ".join(nodes)
-
-
-  def add(self, val):
-    new_node = Node(val)
-    last = self.head
-    if last is None: # proceeds if curr is None
-      self.head = new_node # the node becomes the head
-      new_node.next = None # ends the list
-      return
-
-    while last.next: # if curr is not the last
-      last = last.next
-    last.next = new_node
-
-llist = LinkedList()
-# REMEMBER THIS or else there's no list
-a = Node("HI")
-llist.add(a)
+llist = ListNode()
+llist.add("A")
+llist.add("B")
 print(llist)
