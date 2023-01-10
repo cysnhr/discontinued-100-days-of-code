cda8556c08214b8176ef5afa9119d2144b89b28e
Giselle <113964879+cysnhr@users.noreply.github.com>
2023-01-10 21:23:13 +0800

    Update prime_factor.py

--- codingpractices/prime_factor.py
+++ codingpractices/prime_factor.py
@@ -1,4 +1,33 @@
-# 20230109
+# 20230109-20230110
+
+n = 10000 # int(input())
+fac = []
+i = 2
+# find factors first
+while i < n:
+  if (n % i == 0):
+    print(i, n)
+    fac.append(i)
+    n //= i
+  else:
+    i += 1
+fac.append(n)
+print(fac)
+
+# getting overlapped numbers
+same = []
+for k in fac:
+  if k not in same:
+    same.append(k)
+
+# getting times of repetition
+rep = ""
+for e in same:
+  c = 0
+  if e in fac:
+    c += 1
+  print(f"{e} occurred {c} times")
+
 
 n = int(input())
 fac = []
