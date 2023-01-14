eaf028861cce31fd5de09141592d4b10173e6d56
Giselle <113964879+cysnhr@users.noreply.github.com>
2023-01-14 17:51:51 +0800

    Update linked_list.py

--- codingpractices/linked_list.py
+++ codingpractices/linked_list.py
@@ -16,7 +16,6 @@ class ListNode(object):
         return "No nodes present."
       while node is not None:
         ls.append(str(node.val))
-        print(f"{node} is appended. Pending: {node.next}")
         node = node.next
         continue
         print(ls)
@@ -25,19 +24,15 @@ class ListNode(object):
     # append to the end
     def add(self, val):
       new_node = Node(val)
-      print(f"This is the node you entered: {new_node}: {new_node.val}")
       node = self.head
-      print(f"This is self.head: {self.head}, and the node appointed to it: {node}")
       if node is None: # if self.head is None
         self.head = new_node
         new_node.next = None
-        print(f"The new self.head is {self.head}!")
-        print(f"New node {new_node} added.")
       else:
         while node.next: # while it's not end
           node = node.next
         node.next = new_node
-        print(f"Without None self.head, new node {node.next} added, {node.next.val}.")
+
 
     # reverse
     def backwards(self):
