70929cd7e80d960a26724ae726c1da5b93cdbe27
Giselle <113964879+cysnhr@users.noreply.github.com>
2023-01-22 14:21:29 +0800

    Update budget.py

--- codingpractices/budget.py
+++ codingpractices/budget.py
@@ -1,4 +1,4 @@
-# 20230121
+# 20230121-22
 
 class Category:
   def __init__(self, name, ledger=[], balance=0.00):
@@ -13,8 +13,12 @@ class Category:
     title = f"{'*' * num}{self.name}{'*' * (30-len(self.name)-num)}\n"
     output += title
     for dict in self.ledger:
-      # stuff stuff stuff
-      pass
+      des = dict["description"] 
+      des += " " * 30 # mindlessly adding spaces for slicing later
+      num = '{:.2f}'.format(dict["amount"])
+      des = des[0:29-len(num)] # because the spaces
+      des += " " + '{:.2f}'.format(dict["amount"])
+      output += f"{des}\n"
     return output
   
   def deposit(self, amount: float, description: str=""):
@@ -47,9 +51,34 @@ class Category:
       to_ledger.balance += amount
       return True
 
-def create_spend_chart():
-  pass
 
+def create_spend_chart(categories: list):
+  output = "Percentage spent by category\n"
+  # get total withdrawal
+  total_spent = 0.00
+  for self in categories:
+    for item in self.ledger:
+      if item["amount"] < 0:
+        total_spent += abs(item["amount"]) 
+  total_spent = round(total_spent, 2)
+  
+  # have to get total_spent before retrieving each individual total of ledgers
+  # this is each total of individual categories
+  each_spent = []
+  for self in categories:
+    category_spent = 0
+    for item in self.ledger:
+      if item["amount"] < 0:
+        category_spent += abs(item["amount"])
+    # portion!
+    portion = int(round(100 * (category_spent/total_spent), -1))
+    each_spent.append(portion)
+  
+  # status bar
+  output += ""
+  
+  
+  return each_spent
 
 food = Category("Food")
 food.deposit(1000, "initial deposit")
@@ -64,5 +93,33 @@ auto.deposit(1000, "initial deposit")
 auto.withdraw(15)
 print(food)
 print(clothing)
+print(auto)
+print(create_spend_chart([food, clothing, auto]))
 
-# print(create_spend_chart([food, clothing, auto]))
+"""
+Percentage spent by category
+12345012
+100|          
+ 90|          
+ 80|          
+ 70|          
+ 60| o        
+ 50| o        
+ 40| o        
+ 30| o     
+ 12345678
+ 20| o  o     
+ 10| o  o  o  
+  0| o  o  o  
+    ----------
+     F  C  A  
+     o  l  u  
+     o  o  t  
+     d  t  o  
+        h     
+        i     
+        n     
+        g     
+
+123456789012
+"""
