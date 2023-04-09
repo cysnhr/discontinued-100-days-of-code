05e17be5436d97294f9c18636be918017e649dac
Giselle <113964879+cysnhr@users.noreply.github.com>
2023-04-09 20:38:35 +0800

    Create atoi.c

---
+++ codingpractices/atoi.c
@@ -0,0 +1,41 @@
+// 20230409
+
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int convert(string input, int len);
+
+int main(void)
+{
+    string input = get_string("Enter a positive integer: ");
+
+    // Checks if everything are integers
+    for (int i = 0, n = strlen(input); i < n; i++)
+    {
+        if (!isdigit(input[i]))
+        {
+            printf("Invalid Input!\n");
+            return 1;
+        }
+    }
+
+    // Convert string to int
+    printf("%i\n", convert(input, strlen(input)));
+}
+
+int convert(string input, int len)
+{
+
+    // Base case
+    if (len == 0)
+        return 0; // Stops program
+
+    // Recursive case
+    len -= 1;
+    int digit = input[len] - '0';
+    return convert(input, len) * 10 + digit;
+
+}
